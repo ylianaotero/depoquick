3b7f58cf7dea685d554cff0f0ec78af129256c15
Yliana Otero <yliana.oterocoitino@cencosud.com.uy>
2024-04-15 17:34:03 -0300

    add: GetReservations() - GREEN TDD stage

--- DepoQuick/Domain/Client.cs
+++ DepoQuick/Domain/Client.cs
@@ -5,4 +5,10 @@ public class Client : User
     public Client(string name, string email, string password) : base(name, email, password)
     {
     }
+    
+    public List<Reservation> GetReservations()
+    {
+        return new List<Reservation>();
+    }
 }
+
