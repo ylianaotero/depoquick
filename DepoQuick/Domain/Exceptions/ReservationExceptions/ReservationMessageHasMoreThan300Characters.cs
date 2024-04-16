17d25abbc43b48efe93764576b2c6c58aedcc4c3
MariaBelenR <mariabelen.rywaczuk@gmail.com>
2024-04-16 18:14:19 -0300

    add: tests-reservation-and-exceptions

---
+++ DepoQuick/Domain/Exceptions/ReservationExceptions/ReservationMessageHasMoreThan300Characters.cs
@@ -0,0 +1,6 @@
+﻿namespace DepoQuick.Domain.Exceptions.ReservationExceptions;
+
+public class ReservationMessageHasMoreThan300CharactersException : Exception
+{
+    public ReservationMessageHasMoreThan300CharactersException(string message) : base(message) {}
+}
