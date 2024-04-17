66617012b1a62d25a8f5afb96eb35a9d2da53d26
Yliana Otero <ylianaotero@gmail.com>
2024-04-17 16:42:14 -0300

    add: TestAdministratorCanApproveReservations() - RED TDD stage

--- DepoQuickTests/AdministratorTests.cs
+++ DepoQuickTests/AdministratorTests.cs
@@ -19,5 +19,21 @@ public class AdministratorTests
         Assert.AreEqual(_password, user.GetPassword());
     }
     
-    
+    [TestMethod]
+    public void TestAdministratorCanApproveReservations()
+    {
+        Administrator admin = new Administrator(_name, _email, _password);
+        
+        Client client = new Client("Juan Perez", "juanperez@gmail.com", "JuanP123!Zerep");
+        Deposit deposit = new Deposit('A', "Mediano", true, false);
+        DateRange dateRange = new DateRange(DateTime.Now.AddDays(2), DateTime.Now.AddDays(5));
+        
+        Reservation reservation = new Reservation(deposit, client, dateRange);
+        
+        Assert.IsTrue(reservation.GetSate() == 0);
+        
+        admin.ApproveReservation(reservation);
+        
+        Assert.IsTrue(reservation.GetSate() == 1);
+    }
 }
