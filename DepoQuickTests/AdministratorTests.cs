a333db9dd075d18e88fdedab2fd0d8d315142955
Yliana Otero <ylianaotero@gmail.com>
2024-04-17 18:02:30 -0300

    add: add TestInitialize() and cleanup test - REFACTOR TDD STAGE0

--- DepoQuickTests/AdministratorTests.cs
+++ DepoQuickTests/AdministratorTests.cs
@@ -6,25 +6,51 @@ namespace DepoQuickTests;
 public class AdministratorTests
 {
     private Administrator _admin;
-    private string _name = "Gabriela Pereira";
-    private string _email = "gpereira@depoquick.com";
-    private string _password = "123GabrielaP#";
+    private Client _client;
+    private Deposit _deposit;
+    private Reservation _reservation;
+    
+    private string _adminName = "Gabriela Pereira";
+    private string _adminEmail = "gpereira@depoquick.com";
+    private string _adminPassword = "123GabrielaP#";
+    
+    private string _clientName = "Juan Perez";
+    private string _clientEmail = "jperez@depoquick.com";
+    private string _clientPassword = "321PerezJuan$";
+    
+    
+    [TestInitialize]
+    public void TestInitialize()
+    {
+        _admin = new Administrator(_adminName, _adminEmail, _adminPassword);
+        _client = new Client(_clientName, _clientEmail, _clientPassword);
+        
+        _deposit = new Deposit('A', "Mediano", true, false);
+        
+        DateRange dateRange = new DateRange(DateTime.Now.AddDays(2), DateTime.Now.AddDays(5));
+
+        _reservation = new Reservation(_deposit, _client, dateRange);
+
+    }
+    
+    
+    
     
     [TestMethod]
     public void TestValidAdministratorIsCreated()
     {
-        Administrator user = new Administrator(_name, _email, _password);
-        Assert.AreEqual(_name, user.GetName());
-        Assert.AreEqual(_email, user.GetEmail());
-        Assert.AreEqual(_password, user.GetPassword());
+        Administrator user = new Administrator(_adminName, _adminEmail, _adminPassword);
+        Assert.AreEqual(_adminName, user.GetName());
+        Assert.AreEqual(_adminEmail, user.GetEmail());
+        Assert.AreEqual(_adminPassword, user.GetPassword());
     }
     
     [TestMethod]
     public void TestAdministratorCanApproveReservations()
     {
-        Administrator admin = new Administrator(_name, _email, _password);
+        Administrator admin = new Administrator(_adminName, _adminEmail, _adminPassword);
         
-        Client client = new Client("Juan Perez", "juanperez@gmail.com", _password);
+        Client client = new Client("Juan Perez", "juanperez@gmail.com", _adminPassword);
         Deposit deposit = new Deposit('A', "Mediano", true, false);
         DateRange dateRange = new DateRange(DateTime.Now.AddDays(2), DateTime.Now.AddDays(5));
         
@@ -40,20 +66,12 @@ public class AdministratorTests
     [TestMethod]
     public void TestAdministratorCanRejectReservations()
     {
-        Administrator admin = new Administrator(_name, _email, _password);
-
-        Client client = new Client("Juan Perez", "juanperez@gmail.com", _password);
-        Deposit deposit = new Deposit('A', "Mediano", true, false);
-        DateRange dateRange = new DateRange(DateTime.Now.AddDays(2), DateTime.Now.AddDays(5));
-
-        Reservation reservation = new Reservation(deposit, client, dateRange);
-
-        Assert.IsTrue(reservation.GetSate() == 0 && reservation.GetMessage() == "");
+        Assert.IsTrue(_reservation.GetSate() == 0 && _reservation.GetMessage() == "");
 
         string reason = "No hay disponibilidad.";
 
-        admin.RejectReservation(reservation, reason);
+        _admin.RejectReservation(_reservation, reason);
 
-        Assert.IsTrue(reservation.GetSate() == -1 && reservation.GetMessage() == reason);
+        Assert.IsTrue(_reservation.GetSate() == -1 && _reservation.GetMessage() == reason);
     }
 }
