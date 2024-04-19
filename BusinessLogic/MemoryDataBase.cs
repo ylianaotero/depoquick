d10a0ec42d8ac530815f9218b839ed9c5a173de3
Angelina Maverino <angelinamaverino16@gmail.com>
2024-04-18 21:38:07 -0300

    gestion-of-reservations-interface

--- BusinessLogic/MemoryDataBase.cs
+++ BusinessLogic/MemoryDataBase.cs
@@ -19,15 +19,20 @@ public class MemoryDataBase
         DateTime dayOut = new DateTime(2024, 04, 08);
         DateRange stay = new DateRange(dayIn, dayOut);
         Reservation reservation = new Reservation(deposit, client, stay);
+        
         //agrego usuario para probar pq todavia no esta listo
         _user = new Client("Juan Perez", "nombre@dominio.es", "Contrasena#1"); 
-       // _user = new Administrator("Juan Perez", "nombre@dominio.es", "Contrasena#1");
-       // _user.SetIsAdministrator(true);
+        //_user = new Administrator("Juan Perez", "nombre@dominio.es", "Contrasena#1");
+        //_user.SetIsAdministrator(true);
+        Reservation reservation2 = new Reservation(deposit, (Client)_user, stay);
+        reservation2.SetSate(-1);
         //le estoy poniendo depositos de ejemplo para probar mientras 
         _listOfReservations = new List<Reservation>()
         {
-            reservation
-        }; 
+            reservation,reservation2
+        };
+        Client user = (Client)_user; 
+        user.AddReservation(reservation2);
         _listOfDeposits = new List<Deposit>()
              {
                  new Deposit('a', "Mediano", true, false),
