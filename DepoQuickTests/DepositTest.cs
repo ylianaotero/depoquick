e9e576ec6b6fbc396afa19fcee3ece561ffdb522
Yliana Otero <ylianaotero@gmail.com>
2024-05-05 10:56:51 -0300

    add: TestRemoveReservation

--- DepoQuickTests/DepositTest.cs
+++ DepoQuickTests/DepositTest.cs
@@ -540,4 +540,30 @@ public class DepositTest
         
         CollectionAssert.Contains(newDeposit.GetReservations(), reservation);
     }
+
+
+    [TestMethod]
+    public void TestRemoveReservation()
+    {
+        char area = 'a';
+        String size = "Mediano";
+        bool airConditioning = true;
+        bool reserved = false;
+
+        Deposit newDeposit = new Deposit(area, size, airConditioning, reserved);
+
+        Client client = new Client("Maria Perez", "mariaperez@gmail.com", "Contrasena1#");
+
+        DateTime dayIn = new DateTime(2024, 04, 07);
+        DateTime dayOut = new DateTime(2024, 04, 08);
+        DateRange stay = new DateRange(dayIn, dayOut);
+
+        Reservation reservation = new Reservation(newDeposit, client, stay);
+
+        newDeposit.AddReservation(reservation);
+
+        newDeposit.RemoveReservation(reservation);
+
+        CollectionAssert.DoesNotContain(newDeposit.GetReservations(), reservation);
+    }
 }
