d578cde0d98f4bf5d39c4cab0275615fb4d17fa5
MariaBelenR <mariabelen.rywaczuk@gmail.com>
2024-05-22 22:50:39 -0300

    add: UserControllerContext

--- BusinessLogic/UserController.cs
+++ BusinessLogic/UserController.cs
@@ -1,9 +1,12 @@
-﻿namespace BusinessLogic;
+﻿using SQLitePCL;
+
+namespace BusinessLogic;
 
 public class UserController
 {
+    private DepoQuickContext _context;
     public UserController(DepoQuickContext context)
     {
-        
+        _context = context;
     }
 }
