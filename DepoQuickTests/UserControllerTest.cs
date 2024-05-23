26e224be21acefbc6af7b8bc2cfc9cd08f8fb97d
MariaBelenR <mariabelen.rywaczuk@gmail.com>
2024-05-22 22:46:02 -0300

    add: UserControllerTest

---
+++ DepoQuickTests/UserControllerTest.cs
@@ -0,0 +1,14 @@
+﻿namespace DepoQuickTests;
+
+[TestClass]
+public class UserControllerTest
+{
+    private UserController _userController;
+
+    [TestInitialize]
+    public void Initialize()
+    {
+        var context = TestContextFactory.CreateContext();
+        _userController = new UserController(context);
+    }
+}
