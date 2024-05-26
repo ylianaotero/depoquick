b9c300a0a9ad00ce72d7eaf9c53ac77c2726f966
Angelina Maverino <angelinamaverino16@gmail.com>
2024-05-26 19:32:47 -0300

    add: class payment

---
+++ DepoQuick/Domain/Payment.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using DepoQuick.Exceptions.PaymentExceptions;
+
+namespace DepoQuick.Domain;
+
+public class Payment
+{
+    [Key]
+    public int Id { get; set; }
+    
+    public int Status { get; private set; }
+    
+    
+    public Reservation Reservation { get; set; }
+
+    public Payment()
+    {
+        Status = 0;
+    }
+
+    public void Capture()
+    {
+        if (ThereIsAReservationAssociated())
+        {
+            throw new CannotCapturePaymentIfDoesNotHaveAnAssociatedReservation(
+                "No se puede capturar un pago no asociado con una reservacion"); 
+        }
+        else
+        {
+            Status = 1;
+        }
+    }
+
+    private bool ThereIsAReservationAssociated()
+    {
+        return this.Reservation == null; 
+    }
+}
