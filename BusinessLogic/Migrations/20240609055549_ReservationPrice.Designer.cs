ae7d9ae01d4c2e9cb88503c6940d46a8c0bb41d3
MariaBelenR <mariabelen.rywaczuk@gmail.com>
2024-06-09 03:04:21 -0300

    migration

--- BusinessLogic/Migrations/20240609055549_ReservationPrice.Designer.cs
+++ BusinessLogic/Migrations/20240609055549_ReservationPrice.Designer.cs
@@ -4,6 +4,7 @@ using BusinessLogic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BusinessLogic.Migrations
 {
     [DbContext(typeof(DepoQuickContext))]
-    partial class DepoQuickContextModelSnapshot : ModelSnapshot
+    [Migration("20240609055549_ReservationPrice")]
+    partial class ReservationPrice
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
